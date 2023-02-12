9c9c6593587fcc1d82bc0f8c06e7b86f26f4bf02
Luis Velásquez <luvejo.dev@gmail.com>
2023-02-11 23:58:17 -0500

    test: move Postmark import out of handler

--- api/handler.ts
+++ api/handler.ts
@@ -1,16 +1,15 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { ServerClient } from 'postmark'
 
 export default async function handler(
   _request: VercelRequest,
   response: VercelResponse,
 ) {
-  const postmark = require('postmark')
-
-  const client = new postmark.ServerClient(process.env.POSTMARK_SERVER_TOKEN)
+  const client = new ServerClient(process.env.POSTMARK_SERVER_TOKEN || '')
 
   const postmarkResponse = await client.sendEmail({
-    From: process.env.POSTMARK_SENDER_EMAIL,
-    To: process.env.POSTMARK_RECEIVER,
+    From: process.env.POSTMARK_SENDER_EMAIL || '',
+    To: process.env.POSTMARK_RECEIVER || '',
     Subject: 'Test',
     TextBody: 'Postmark works!',
   })
